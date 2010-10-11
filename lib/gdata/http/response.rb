a028d3cccec84b22a4b4c97773f37f2c32a518e6
Corné Verbruggen <corne@g-majeur.nl>
2010-10-11 16:38:21 +0200

    require rake in gemspec
    Use Nokogiri instead of REXML

--- lib/gdata/http/response.rb
+++ lib/gdata/http/response.rb
@@ -27,11 +27,11 @@ module GData
       # The headers of the HTTP response.
       attr_accessor :headers
       
-      # Converts the response body into a REXML::Document
+      # Converts the response body into a Nokogiri::XML document
       def to_xml
         if @body
           begin
-            return REXML::Document.new(@body).root
+            return Nokogiri::XML(@body).root
           rescue
             raise GData::Client::Error, "Response body not XML."
           end
